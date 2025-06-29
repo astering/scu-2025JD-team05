f88e753a61d2fde0b76fcf5b581a32c95d38b34f
ar45 <1079671858@qq.com>
2025-06-29 14:53:41 +0800

    新增年度歌曲风格变化趋势

---
+++ dags/node2/genre_yearly_dag.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.hooks.base import BaseHook
+
+SPARK_SCRIPTS_PATH = "airflow/dags/spark_etl_pipeline/scripts"
+EVENT_PATH = "hdfs://node-master:9000/mir/ThirtyMusic/relations/events.idomaar"
+TRACK_PATH = "hdfs://node-master:9000/mir/ThirtyMusic/entities/tracks.idomaar"
+LASTFM_PATH = "file:///mir/lastfm_subset"  # 可根据挂载调整
+
+MYSQL_CONN_ID = "mysql_ads_db2"
+MYSQL_TARGET_TABLE = "genre_yearly_stat"
+MYSQL_DRIVER = "com.mysql.jdbc.Driver"
+
+mysql_conn = BaseHook.get_connection(MYSQL_CONN_ID)
+mysql_jdbc_url = f"jdbc:mysql://{mysql_conn.host}:{mysql_conn.port}/{mysql_conn.schema}"
+mysql_user = mysql_conn.login
+mysql_password = mysql_conn.password
+
+with DAG(
+    dag_id="genre_yearly_etl_to_mysql",
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Shanghai"),
+    catchup=False,
+    schedule=None,
+    tags=["spark", "mysql", "genre"]
+) as dag:
+    start = EmptyOperator(task_id="start")
+
+    genre_stat_task = SparkSubmitOperator(
+        task_id="spark_genre_yearly_to_mysql",
+        application=f"{SPARK_SCRIPTS_PATH}/genre_yearly_to_mysql.py",
+        conn_id="spark_default",
+        application_args=[
+            EVENT_PATH,
+            TRACK_PATH,
+            LASTFM_PATH,
+            mysql_jdbc_url,
+            mysql_user,
+            mysql_password,
+            MYSQL_DRIVER,
+            MYSQL_TARGET_TABLE
+        ],
+        name="genre_yearly_etl_{{ ds_nodash }}",
+        verbose=True,
+        conf={"spark.driver.memory": "2g"},
+        executor_cores=1,
+        executor_memory="2g",
+        num_executors=2
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> genre_stat_task >> end
