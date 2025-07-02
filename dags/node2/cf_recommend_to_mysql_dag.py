69464e629b6337663da130a5507f0aca5a58b5b1
ar45 <1079671858@qq.com>
2025-07-02 17:09:09 +0800

    尝试修复Mysql连接错误的问题

--- dags/node2/cf_recommend_to_mysql_dag.py
+++ dags/node2/cf_recommend_to_mysql_dag.py
@@ -19,7 +19,7 @@ LOVE_PATH = "hdfs://node-master:9000/user/hive/warehouse/ods_love"
 
 # 读取 Airflow 中配置的 MySQL 连接
 conn = BaseHook.get_connection("mysql_ads_db2")
-MYSQL_URL = f"jdbc:mysql://{conn.host}:{conn.port}/{conn.schema}?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai"
+MYSQL_URL = f"jdbc:mysql://{conn.host}:{conn.port}/{conn.schema}?useSSL=false&serverTimezone=UTC"
 
 with DAG(
     dag_id="cf_recommend_to_mysql_dag",
