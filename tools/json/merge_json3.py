f4c4457a7ba503d18d771ed7827a335d0b8788bd
dawn <131938961+astering@users.noreply.github.com>
2025-07-04 20:34:47 +0800

    合并json

--- tools/json/merge_json3.py
+++ tools/json/merge_json3.py
@@ -39,10 +39,12 @@ def get_file_metadata(path):
 allowed_extensions = {'.json', '.txt'}  # 可添加其他扩展名
 
 def merge_json_files(input_dir, output_file):
-    first = True
     try:
         with open(output_file, 'w', encoding='utf-8') as out_f:
-            out_f.write('[\n')
+            buffer = ''
+            file_count = 0
+            counter1 = 0
+            counter2 = 0
             for root, dirs, files in os.walk(input_dir):
                 for filename in files:
                     if filename.endswith('.json'):
@@ -53,16 +55,27 @@ def merge_json_files(input_dir, output_file):
                                 # 使用json.load+dump，可以不提前线性化处理
                                 data = json.load(f)
                                 # if needed: data["_source"] = os.path.relpath(file_path, input_dir)
-                                if not first:
-                                    # out_f.write(',') # 单行
-                                    out_f.write(',\n') # 合并的每一个文件换一行
-                                else:
-                                    first = False
-                                json.dump(data, out_f, ensure_ascii=False)
-                                print(f"已合并: {file_path} (相对路径: {os.path.relpath(file_path, input_dir)})")
+                                # buffer.join([json.dumps(data, ensure_ascii=False), '\n'])
+                                buffer += json.dumps(data, ensure_ascii=False)
+                                buffer += '\n'
+                                # print(data)
+                                counter1 += 1
+                                if counter1 % 10 == 0:
+                                    print(f"{counter1/10}%")
+                                file_count += 1
+                                if file_count == 1000:
+                                    file_count = 0
+                                    counter1 = 0
+                                    # print(buffer)
+                                    out_f.write(buffer)
+                                    buffer = ''
+                                    counter2 += 1
+                                    print("满1000条写入1次", counter2/1000)
+                                # print(f"已合并: {file_path} (相对路径: {os.path.relpath(file_path, input_dir)})")
                         except Exception as e:
                             print(f"警告: 处理文件 {filename} 时出错 - {str(e)}")
-            out_f.write('\n]')
+            out_f.write(buffer)
+            print("写入剩余部分")
         print(f"\n成功合并文件到 {output_file}")
         return True
     except Exception as e:
